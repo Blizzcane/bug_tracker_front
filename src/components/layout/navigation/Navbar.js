ff809ba0ae694c4a2e9d3b81fd12295fda2fbde5
Blizzcane <blizzcane@gmail.com>
2021-10-10 13:34:39 -0500

    made some components and added bootstrap

---
+++ src/components/layout/navigation/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <p>
+            nav bar will go here
+        </p>
+  );
+}
+
+export default Navbar;
---
+++ src/views/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/layout/navigation/Navbar";
+
+function Dashboard() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Dash will go here</h1>
+        </div>
+  );
+}
+
+export default Dashboard;
