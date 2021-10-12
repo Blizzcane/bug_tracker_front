dfe25e84e0dae917df2b49c93150d48c8b8084d6
Blizzcane <blizzcane@gmail.com>
2021-10-11 23:37:20 -0500

    working on filterbar

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Navbar from "../components/layout/navigation/Navbar";
+import './Dashboard.css'
 
 function Dashboard() {
-    return (
-        <div>
-            <Navbar />
-            <h1>Dash will go here</h1>
+  return (
+    <div>
+      <Navbar />
+      <h1>Dash will go here</h1>
+      <div class="wrap">
+        <div class="search">
+          <input
+            type="text"
+            className="searchTerm"
+            placeholder="What are you looking for?"
+          />
+          <button type="submit" className="searchButton">
+            <i class="fa fa-search"></i>
+          </button>
         </div>
+      </div>
+    </div>
   );
 }
 
