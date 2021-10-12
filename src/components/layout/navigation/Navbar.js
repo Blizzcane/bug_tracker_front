ad79a0809ded7472429839b41835c8b8f9019ef5
Blizzcane <blizzcane@gmail.com>
2021-10-11 22:24:05 -0500

    navbar changes

--- src/components/layout/navigation/Navbar.js
+++ src/components/layout/navigation/Navbar.js
@@ -1,10 +1,39 @@
 import React from "react";
 
 function Navbar() {
-    return (
-        <p>
-            nav bar will go here
-        </p>
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Bug Tracker
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Username
+              </a>
+            </li>
+            <li className="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                Log Out
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 }
 
