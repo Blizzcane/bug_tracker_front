8cc09bdbb3d55c9d55dc377e77f1cf1e6654b840
Blizzcane <blizzcane@gmail.com>
2021-10-12 23:47:21 -0500

    added table

--- src/components/layout/navigation/ProjectTable.js
+++ src/components/layout/navigation/ProjectTable.js
@@ -2,22 +2,36 @@ import React from "react";
 
 function ProjectTable() {
   return (
-    <table>
-      <tr>
-        <th>Company</th>
-        <th>Contact</th>
-        <th>Country</th>
-      </tr>
-      <tr>
-        <td>Alfreds Futterkiste</td>
-        <td>Maria Anders</td>
-        <td>Germany</td>
-      </tr>
-      <tr>
-        <td>Centro comercial Moctezuma</td>
-        <td>Francisco Chang</td>
-        <td>Mexico</td>
-      </tr>
+    <table class="table ">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Bugs</th>
+          <th scope="col">Members</th>
+          <th scope="col">Admin</th>
+          <th scope="col">Added</th> 
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
