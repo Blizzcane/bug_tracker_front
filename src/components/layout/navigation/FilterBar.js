07c4ac122974ca26a26ab91244dae6401fda0f28
Blizzcane <blizzcane@gmail.com>
2021-10-12 23:29:56 -0500

    working on layout

--- src/components/layout/navigation/FilterBar.js
+++ src/components/layout/navigation/FilterBar.js
@@ -1,13 +1,13 @@
 import React from "react";
-import './FilterBar.css'
+import "./FilterBar.css";
 
-const myFunction = () => {
+const filterProjects = () => {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
+  input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+  li = ul.getElementsByTagName("li");
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
@@ -19,7 +19,39 @@ const myFunction = () => {
       li[i].style.display = "none";
     }
   }
-}
+};
+
+const sortList = () => {
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("myUL");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    // Loop through all list items:
+    for (i = 0; i < b.length - 1; i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Check if the next item should
+      switch place with the current item: */
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        /* If next item is alphabetically lower than current item,
+        mark as a switch and break the loop: */
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+};
 
 function FilterBar() {
   return (
@@ -27,19 +59,22 @@ function FilterBar() {
       <input
         type="text"
         id="myInput"
-        onkeyup="myFunction()"
+        onKeyUp={filterProjects}
         placeholder="Search for names.."
       />
+      <button id="addProject" onClick={sortList} type="button">
+        Add Project
+      </button>
       <ul id="myUL">
         <li>
           <a href="#">Adele</a>
         </li>
-        <li>
-          <a href="#">Agnes</a>
-        </li>
 
         <li>
           <a href="#">Billy</a>
+        </li>
+        <li>
+          <a href="#">Agnes</a>
         </li>
         <li>
           <a href="#">Bob</a>
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/layout/navigation/Navbar";
 import FilterBar from "../components/layout/navigation/FilterBar"
+import ProjectTable from "../components/layout/navigation/ProjectTable";
 import './Dashboard.css'
 
 function Dashboard() {
@@ -8,7 +9,8 @@ function Dashboard() {
     <div>
       <Navbar />
       <h1>Dash will go here</h1>
-       <FilterBar />
+      <FilterBar />
+      <ProjectTable />
     </div>
   );
 }
