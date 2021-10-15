ad3ce34557709131cea38f3cef9f0f0e69cfeb4d
Blizzcane <blizzcane@gmail.com>
2021-10-14 22:50:35 -0500

    adding api functions

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/layout/navigation/Navbar";
 import FilterBar from "../components/layout/navigation/FilterBar";
 import ProjectTable from "../components/layout/navigation/ProjectTable";
 import "./Dashboard.css";
+import { loadProjects } from "../utils/api";
 
 function Dashboard() {
   const test = [
@@ -21,6 +22,11 @@ function Dashboard() {
   ];
 
   const [projects, setProjects] = useState(test);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    loadProjects(abortController.signal).then(setProjects);
+  }, []);
 
   return (
     <div>
