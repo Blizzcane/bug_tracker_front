3e83057e4c3fb6efa4f73d11103c14551cf2f73a
Blizzcane <blizzcane@gmail.com>
2021-10-17 23:54:37 -0500

    styling modal

--- src/components/layout/navigation/AddProject.js
+++ src/components/layout/navigation/AddProject.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./AddProject.css"
 
 function AddProject({toggleModal}) {
-    return <div>
+    return <div >
         <h2>hey</h2>
         <button  onClick={toggleModal} type="button">
         close
