6d2366a4e2dc8fbbc8e9712c60b117b0bff83f1a
Blizzcane <blizzcane@gmail.com>
2021-10-20 00:00:29 -0500

    fixed modal issue

--- src/components/layout/navigation/AddProject.js
+++ src/components/layout/navigation/AddProject.js
@@ -1,13 +1,29 @@
 import React from "react";
-import "./AddProject.css"
+import ReactDom from "react-dom";
+import "./AddProject.css";
 
-function AddProject({toggleModal}) {
-    return <div >
-        <h2>hey</h2>
-        <button  onClick={toggleModal} type="button">
-        close
-      </button>
-  </div>;
+function AddProject({ toggleModal }) {
+  return ReactDom.createPortal(
+    <div className="overlay">
+      <div className="addProject">
+        <h2>Add a new project</h2>
+        <input type="text" placeholder="Project Name" />
+        <select name="select-members" id="select-members">
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+        <button className="close" onClick={toggleModal} type="button">
+          x
+        </button>
+        <button className="create"  type="button">
+          Create New Project
+        </button>
+      </div>
+    </div>,
+    document.getElementById("portal")
+  );
 }
 
 export default AddProject;
