8feb3d72e2d2f8d271fbb58ee765a22a4449a8c9
Blizzcane <blizzcane@gmail.com>
2021-10-21 21:59:54 -0500

    new modal with bootstrap, having issues

--- src/components/layout/navigation/AddProject.js
+++ src/components/layout/navigation/AddProject.js
@@ -3,41 +3,11 @@ import ReactDom from "react-dom";
 import "./AddProject.css";
 
 function AddProject({ toggleModal }) {
-  return ReactDom.createPortal(
-    <div className="overlay">
-      <div className="addProject">
-        <h5 class="modal-title">Add a new project</h5>
-
-        <div class="modal-body">
-          <p>Modal body text goes here.</p>
-        </div>
-        <form>
-          <div class="mb-3">
-            <label for="recipient-name" class="col-form-label">
-              Recipient:
-            </label>
-            <input type="text" class="form-control" id="recipient-name" />
-          </div>
-          <div class="mb-3">
-            <label for="message-text" class="col-form-label">
-              Message:
-            </label>
-            <textarea class="form-control" id="message-text"></textarea>
-          </div>
-        </form>
-
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" onClick={toggleModal}>
-            Close
-          </button>
-
-          <button type="button" class="btn btn-primary">
-            Create New Project
-          </button>
-        </div>
-      </div>
-    </div>,
-    document.getElementById("portal")
+  return (
+    <div className="modalBackground">
+      <div className="modelContainer">Modal</div>
+      
+    </div>
   );
 }
 
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -8,8 +8,8 @@ import AddProject from "../components/layout/navigation/AddProject";
 
 function Dashboard() {
   const [projects, setProjects] = useState([]);
-  const [projectErrors, setProjectErrors] = useState([]);
-  const [showModal, setShowModal] = useState(false);
+  const [projectErrors, setProjectErrors] = useState([]); 
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     const abortController = new AbortController();
@@ -18,24 +18,28 @@ function Dashboard() {
       .catch(setProjectErrors);
   }, []);
 
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
-
   return (
     <div>
       <Navbar />
-      {showModal ? <AddProject toggleModal={toggleModal} /> : null}
+      {openModal && <AddProject />}
       <div>
         <h2>All Projects </h2>
         <h5>List of all the created or joined projects</h5>
       </div>
 
       <FilterBar projects={projects} setProjects={setProjects} />
-      <button id="addProject" onClick={toggleModal} type="button">
+      <button id="addProject" onClick={() => setOpenModal(true)} type="button">
         Add Project
       </button>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        data-bs-whatever="@mdo"
+      >
+        Open modal for @mdo
+      </button>
 
       <ProjectTable projects={projects} />
     </div>
