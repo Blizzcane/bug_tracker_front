32ec34306fb10d4db449892dc0b3bb83090a75ff
Blizzcane <blizzcane@gmail.com>
2021-12-09 20:43:58 -0600

    adding formData handlers

--- src/components/layout/navigation/AddProject.js
+++ src/components/layout/navigation/AddProject.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 import "./AddProject.css";
 
 function AddProject({ setOpenModal }) {
+  const [formData, setFormData] = useState({ members: [], projectName: "" });
+
   const members = [
     { value: "chocolate", label: "Chocolate" },
     { value: "strawberry", label: "Strawberry" },
@@ -10,23 +12,44 @@ function AddProject({ setOpenModal }) {
     { value: "lemon", label: "Lemon" },
   ];
 
+  const createProject = (e) => {
+    e.preventDefault();
+    setFormData("aaa");
+
+    console.log(formData);
+  };
+
+  const handleChange = ({ target }) => {
+    setFormData({
+      ...formData,
+      projectName: target.value,
+    });
+    console.log(formData);
+  };
+
   return (
     <div className="overlay">
       <div className="addProject">
         <button onClick={() => setOpenModal(false)} id="closeBtn">
           x
         </button>
-        
+
         <h3>Add a new project</h3>
-        <input type="text" placeholder="Project name" />
+        <input
+          type="text"
+          placeholder="Project name"
+          onChange={handleChange}
+          value={formData.projectName}
+        />
         <Select
           isMulti
           className="basic-multi-select"
           classNamePrefix="select"
           options={members}
+          value={formData.members}
         />
 
-        <button>CREATE NEW PROJECT</button>
+        <button onClick={createProject}>CREATE NEW PROJECT</button>
       </div>
     </div>
   );
